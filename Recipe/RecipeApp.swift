cca74f0dabe02ee31e1c5dd676bea728909ded0d
adamcaak <ladamczak1997@gmail.com>
2025-01-08 22:01:04 +0100

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Łukasz Adamczak on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
