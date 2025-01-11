c74dc478e397d009257883ca433e9bc6c3eb1ad1
adamcaak <ladamczak1997@gmail.com>
2025-01-11 21:49:39 +0100

    Change var

--- Recipe/ViewModels/RecipeData.swift
+++ Recipe/ViewModels/RecipeData.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 class RecipeData: ObservableObject {
-  @Published var recipe = Recipe.testRecipes
+  @Published var recipes = Recipe.testRecipes
     
     func recipes(for category: MainInformation.Category) -> [Recipe] {
         var filteredRecipes = [Recipe]()
         
-        for recipe in self.recipe {
+        for recipe in recipes {
             if recipe.mainInformation.category == category {
                 filteredRecipes.append(recipe)
             }
