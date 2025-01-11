d054411d1983341530e9d49913cf3661d9d40303
adamcaak <ladamczak1997@gmail.com>
2025-01-11 21:43:29 +0100

    RecipeData
    Solved error

--- Recipe/ViewModels/RecipeData.swift
+++ Recipe/ViewModels/RecipeData.swift
@@ -9,14 +9,15 @@ import Foundation
 
 class RecipeData: ObservableObject {
   @Published var recipe = Recipe.testRecipes
-  
+    
     func recipes(for category: MainInformation.Category) -> [Recipe] {
         var filteredRecipes = [Recipe]()
         
-        for recipe in recipeData.recipes {
+        for recipe in self.recipe {
             if recipe.mainInformation.category == category {
                 filteredRecipes.append(recipe)
             }
         }
+        return filteredRecipes
     }
 }
--- Recipe/Views/RecipesListView.swift
+++ Recipe/Views/RecipesListView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct RecipesListView: View {
     @EnvironmentObject private var recipeData: RecipeData
     let category: MainInformation.Category
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
     
     var body: some View {
         List {
             ForEach(recipes) { recipe in
                 NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
-            //.listRowBackground(listBackgroundColor)
-            //.foregroundColor(listTextColor)
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle(navigationTitle)
     }
