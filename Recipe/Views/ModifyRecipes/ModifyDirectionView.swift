a723cd8b133c5d8835dcbbdd48029373dfb337d0
adamcaak <ladamczak1997@gmail.com>
2025-01-17 18:38:23 +0100

    Add
    Add new file, and create direction view

---
+++ Recipe/Views/ModifyRecipes/ModifyDirectionView.swift
@@ -0,0 +1,44 @@
+//
+//  ModifyDirectionView.swift
+//  Recipe
+//
+//  Created by Łukasz Adamczak on 17/01/2025.
+//
+
+import SwiftUI
+
+struct ModifyDirectionView: View {
+    @Binding var direction: Direction
+    let createAction: (Direction) -> Void
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    @Environment(\.presentationMode) private var mode
+    var body: some View {
+        Form {
+            TextField("Direction description", text: $direction.description)
+                .listRowBackground(listBackgroundColor)
+            Toggle("Optional", isOn: $direction.isOptional)
+                .listRowBackground(listBackgroundColor)
+            HStack {
+                Spacer()
+                Button("Save") {
+                    createAction(direction)
+                    mode.wrappedValue.dismiss()
+                }
+                Spacer()
+            }.listRowBackground(listBackgroundColor)
+        }
+        .foregroundColor(listTextColor)
+    }
+}
+
+struct ModifyDirectionView_Previews: PreviewProvider {
+    @State static var emptyDirection = Direction(description: "", isOptional: false)
+    static var previews: some View {
+        NavigationView {
+            ModifyDirectionView(direction: $emptyDirection) { _ in return }
+        }
+    }
+}
