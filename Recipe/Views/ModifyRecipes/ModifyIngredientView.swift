d5b5d5f2d63592576246045ba0f20d82f5c6570e
adamcaak <ladamczak1997@gmail.com>
2025-01-17 19:02:55 +0100

    Modify Ingredient View
    Modification struct and add init

--- Recipe/Views/ModifyRecipes/ModifyIngredientView.swift
+++ Recipe/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
-struct ModifyIngredientView: View {
+struct ModifyIngredientView: ModifyComponentView{
     @Binding var ingredient: Ingredient
     let createAction: ((Ingredient) -> Void)
+    
+    init(component: Binding<Ingredient>, createAction: @escaping (Ingredient) -> Void) {
+        self._ingredient = component
+        self.createAction = createAction
+    }
     
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
